bd0187872363aba4e68d550ec439b20bd6da9096
Michael Trittin <michaeltrittin@gmail.com>
2024-07-30 16:32:30 -0400

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,47 @@
+function Run-Branches {
+    & (resolve-path "~/scripts/branches.ps1").Path
+}
+
+function Write-BranchName () {
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+
+        if ($branch -eq "HEAD") {
+            # we're probably in detached HEAD state, so print the SHA
+            $branch = git rev-parse --short HEAD
+            Write-Host " ($branch)" -ForegroundColor "red"
+        }
+        else {
+            # we're on an actual branch, so print it
+            Write-Host " ($branch)" -ForegroundColor "blue"
+        }
+    } catch {
+        # we'll end up here if we're in a newly initiated git repo
+        Write-Host " (no branches yet)" -ForegroundColor "yellow"
+    }
+}
+
+function prompt {
+    $base = "PS "
+    $path = "$($executionContext.SessionState.Path.CurrentLocation)"
+    $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
+
+    Write-Host "`n$base" -NoNewline
+
+    if (Test-Path .git) {
+        Write-Host $path -NoNewline -ForegroundColor "green"
+        Write-BranchName
+    }
+    else {
+        # we're not in a repo so don't bother displaying branch name/sha
+        Write-Host $path -ForegroundColor "green"
+    }
+
+    return $userPrompt
+}
+
+function Clear-Host { 
+  Write-Host -NoNewLine "$([char]27)[2J$([char]27)[3J$([char]27)[H" 
+}
+
+Set-Alias -Name "branches" -Value "Run-Branches"
